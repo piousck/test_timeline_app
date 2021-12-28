1dcceda12f929807dabdbbba9619dcb6f5e5f466
piousck <pious.james007@gmail.com>
2021-12-28 16:12:32 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => NavigationProvider(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(),
+        home: MyHomePage(),
       ),
     );
   }
--- lib/pages.dart
+++ lib/pages.dart
@@ -4,19 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:test_timeline_app/components/sidebar.dart';
 import 'package:collapsible_sidebar/collapsible_sidebar.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var now = DateTime.now();
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 10,
-          backgroundColor: Colors.amber,
-        ),
-        drawer: NavigationDrawer(),
-        body: const Text("Contents"),
-        );
-        
+      
+      appBar: AppBar(
+        toolbarHeight: 10,
+        backgroundColor: Colors.amber,
+      ),
+      persistentFooterButtons: [Text("$now")],
+      body:
+          Container(alignment: Alignment.centerLeft, child: NavigationDrawer()),
+    );
   }
 }
